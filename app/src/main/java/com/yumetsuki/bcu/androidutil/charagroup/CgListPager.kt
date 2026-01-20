124436ab79d128f5f71798fcca7fdc8b26ac658d
Israel <Catsaber9999@gmail.com>
2026-01-20 14:53:42 -0400

    0.20.19 (real)
    sucks to suck

--- app/src/main/java/com/yumetsuki/bcu/androidutil/charagroup/CgListPager.kt
+++ app/src/main/java/com/yumetsuki/bcu/androidutil/charagroup/CgListPager.kt
@@ -1,14 +1,22 @@
 package com.yumetsuki.bcu.androidutil.charagroup
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.yumetsuki.bcu.R
+import common.pack.Identifier
+import common.pack.PackData
+import common.pack.UserProfile
 
 class CgListPager : Fragment() {
 
     companion object {
         fun newInstance(pid: String) : CgListPager {
             val cs = CgListPager()
-
             val bundle = Bundle()
 
             bundle.putString("pid", pid)
@@ -16,5 +24,21 @@ class CgListPager : Fragment() {
 
             return cs
         }
+    }
+    private var pid = Identifier.DEF
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val c = context ?: return null
+        val view = inflater.inflate(R.layout.entity_list_pager, container, false)
+
+        pid = arguments?.getString("pid") ?: Identifier.DEF
+
+        val list = view.findViewById<ListView>(R.id.entitylist)
+        val nores = view.findViewById<TextView>(R.id.entitynores)
+
+        val p = UserProfile.getPack(pid)
+        var index = -1
+
+        return view
     }
 }
